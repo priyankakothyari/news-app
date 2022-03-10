6acc64ddca58d62d11f35bdf68993832089b29df
priyankakothyari <98269430+priyankakothyari@users.noreply.github.com>
2022-03-10 08:17:14 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -35,9 +35,7 @@ export class News extends Component {
             totalResults:result.totalResults
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
+       
         (error) => {
           this.setState({
             isLoaded: true,
